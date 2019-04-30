1ecf4a57b0b2b109c6abbcc384b657d986476216
dwilley21 <daltonwilley@gmail.com>
2019-04-29 23:35:21 -0500

    added testimonials array for landing page

---
+++ Javascript/index.js
@@ -0,0 +1,29 @@
+const testimonials = [
+   {
+       Name: 'Shaun',
+       Quote: 'lorem asojlfasofjaobp jaoijaf',
+       Image: 'Assets/mentorship.jpg',
+       Business: 'Value Tools',
+   },
+
+   {
+    Name: 'Paula',
+    Quote: 'lorem asojlfasofjaobp jaoijaf',
+    Image: 'Assets/mentorship.jpg',
+    Business: 'Prestige Consulting',
+},
+
+{
+    Name: 'Juan',
+    Quote: 'lorem asojlfasofjaobp jaoijaf',
+    Image: 'Assets/mentorship.jpg',
+    Business: 'Rodriguez Law Firm',
+},
+
+{
+    Name: 'Dave',
+    Quote: 'lorem asojlfasofjaobp jaoijaf',
+    Image: 'Assets/mentorship.jpg',
+    Business: 'Hardware Heroes',
+},
+    ]
