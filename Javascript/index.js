cd634f707ba77055c9e683ed9479247fc14c95c2
dwilley21 <daltonwilley@gmail.com>
2019-05-02 18:32:30 -0500

    added accordion for about

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,31 +1,62 @@
 
-const carousel = document.querySelector("[data-target='carousel']");
-const card = carousel.querySelector("[data-target='card']");
-const leftButton = document.querySelector("[data-action='slideLeft']");
-const rightButton = document.querySelector("[data-action='slideRight']");
+// const carousel = document.querySelector("[data-target='carousel']");
+// const card = carousel.querySelector("[data-target='card']");
+// const leftButton = document.querySelector("[data-action='slideLeft']");
+// const rightButton = document.querySelector("[data-action='slideRight']");
 
-const carouselWidth = carousel.offsetWidth;
-const cardStyle = card.currentStyle || window.getComputedStyle(card)
-const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
+// const carouselWidth = carousel.offsetWidth;
+// const cardStyle = card.currentStyle || window.getComputedStyle(card)
+// const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
 
-const cardCount = carousel.querySelectorAll("[data-target='card']").length;
+// const cardCount = carousel.querySelectorAll("[data-target='card']").length;
 
-let offset = 0;
-const maxX = -((cardCount / 3) * carouselWidth + 
-               (cardMarginRight * (cardCount / 3)) - 
-               carouselWidth - cardMarginRight);
+// let offset = 0;
+// const maxX = -((cardCount / 3) * carouselWidth + 
+//                (cardMarginRight * (cardCount / 3)) - 
+//                carouselWidth - cardMarginRight);
 
 
-leftButton.addEventListener("click", function() {
-  if (offset !== 0) {
-    offset += carouselWidth + cardMarginRight;
-    carousel.style.transform = `translateX(${offset}px)`;
-    }
-})
+// leftButton.addEventListener("click", function() {
+//   if (offset !== 0) {
+//     offset += carouselWidth + cardMarginRight;
+//     carousel.style.transform = `translateX(${offset}px)`;
+//     }
+// })
   
-rightButton.addEventListener("click", function() {
-  if (offset !== maxX) {
-    offset -= carouselWidth + cardMarginRight;
-    carousel.style.transform = `translateX(${offset}px)`;
-  }
-})
+// rightButton.addEventListener("click", function() {
+//   if (offset !== maxX) {
+//     offset -= carouselWidth + cardMarginRight;
+//     carousel.style.transform = `translateX(${offset}px)`;
+//   }
+// })
+
+
+//accordion 
+
+function  accordion () {
+  const accordion = document.querySelector('.accordion');
+  const accordionTitles = accordion.querySelectorAll('accordionTitle');
+
+  [].forEach(accordionTitles, function(accordionTitle){
+    accordeonTitle.addEventListener('click', function() {
+      const activeTitle = accordion.querySelector('.accordionTitle--active');
+      const activeDescript = accordion.querySelector('accordionDescription.accordionDescription--visible');
+      const clickedDescription = accordionTitle.parentElement.querySelector('accordionDescription');
+
+      if(activeDescript){
+        activeDescript.classList.remove('accordionTitle--active');
+      }
+      accordionTitle.classList.toggle('accordionTitle--active');
+
+      if (activeDescript){
+        activeDescript.classList.remove('accordionDescription--visible');
+        activeDescript.style.maxHeight = null; 
+      }
+
+      clickedDescription.classList.toggle('accordionDescription--visible');
+      clickedDescription.style.maxHeight = clickedDescription.scrollHeigh + "px";
+    });
+  });
+};
+
+accordion();
